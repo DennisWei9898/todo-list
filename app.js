ae87968290ef5771a0c176efd69b8286e2ba5c1a
Dennis Wei <denniseasycard@gmail.com>
2021-04-09 23:08:39 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
+const exphbs = require('express-handlebars')
+const Todo =require('./models/todo')
+
 const mongoose = require('mongoose')
 const app = express()
 
@@ -16,9 +19,20 @@ db.once('open',()=>{
   console.log('mongodb connected!')
 })
 
+//設定引擎
+
+app.engine('hbs',exphbs({defaultLayout:'main',extname:'.hbs'}))
+app.set('view engine','hbs')
+
+
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.send('hello world')
+
+  //拿到全部的todo資料
+  Todo.find()
+      .lean()
+      .then(todos=>res.render('index',{todos}))
+      .catch(error=>console.log(error))
 })
 
 // 設定 port 3000
