f9079035da570f6610d88dab8dd36ede3d4805be
Dennis Wei <denniseasycard@gmail.com>
2021-04-19 10:30:17 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
-// 載入 express 並建構應用程式伺服器
+// 載入相關套件
 const express = require('express')
 const exphbs = require('express-handlebars')
-
 const methodOverride = require('method-override')
-
 const bodyParser = require('body-parser')
 
-const Todo = require('./models/todo')
-
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
-
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // 設定引擎
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(methodOverride('_method'))
-app.use(routes)
 
+app.use(routes)
 
 // 設定 port 3000
 app.listen(3000, () => {
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,21 +1,10 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo')
 
-mongoose.connect('mongodb://localhost/todo-list',{ useNewUrlParser: true ,useUnifiedTopology:true})
+const db = require('../../config/mongoose')
 
-const db = mongoose.connection
-
-db.on('error',()=>{
-  console.log('mongodb error!')
-})
-
-db.once('open',()=>{
-
-  console.log('mongodb connected!')
-
-  for(let i=0; i<10;i++){
-    Todo.create({name: `name-${i}`})
-
+db.once('open', () => {
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: `name-${i}` })
   }
-  console.log(`done.`)
+  console.log('done.')
 })
